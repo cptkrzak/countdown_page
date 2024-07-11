e5c70a657ff31e92092ae7214aec8664412d730d
cptkrzak <krzak.szymon@gmail.com>
2024-07-11 20:57:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Data końcowa
+var countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 3);
+countDownDate.setHours(countDownDate.getHours() + 17);
+countDownDate.setMinutes(countDownDate.getMinutes() + 9);
+countDownDate.setSeconds(countDownDate.getSeconds() + 50);
+
+// Aktualizacja odliczania co sekundę
+var countdownFunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "KONIEC";
+    }
+}, 1000);
+
+// Odtwarzanie muzyki
+document.getElementById('background-music').play();
